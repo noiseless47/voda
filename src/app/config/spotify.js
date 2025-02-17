2a6a0c9166aa6e459868385ccfd6f9ae2e25b276
noiseless47 <asishyeleti2005@gmail.com>
2025-02-17 20:42:54 +0530

    Fixed Bugs

--- src/app/config/spotify.js
+++ src/app/config/spotify.js
@@ -1,6 +1,6 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
-export const clientId = "YOUR_CLIENT_ID"; // Get this from Spotify Developer Dashboard
-export const redirectUri = "http://localhost:3000";
+export const clientId = NEXT_PUBLIC_SPOTIFY_CLIENT_ID; // Get this from Spotify Developer Dashboard
+export const redirectUri = NEXT_PUBLIC_REDIRECT_URI;
 export const scopes = [
   "user-read-currently-playing",
   "user-read-recently-played",
